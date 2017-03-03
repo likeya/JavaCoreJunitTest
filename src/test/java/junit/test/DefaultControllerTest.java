4b77fbab2be4389de00f54ac728bc4656a78b433
LiKeYa <995405013@qq.com>
2017-03-03 14:55:51 +0800

    '对在AddHandlerTest()和ProcessRequest()方法中重复使用的实例化代码进行重构'

--- src/test/java/junit/test/DefaultControllerTest.java
+++ src/test/java/junit/test/DefaultControllerTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.assertSame;
  */
 public class DefaultControllerTest {
     private DefaultController controller;
+    private Request request;
+    private RequestHandler handler;
 
     private class SampleRequest implements Request {
         public String getName() {
@@ -34,10 +36,16 @@ public class DefaultControllerTest {
 
     }
 
+    /*
+    * 对在AddHandlerTest()和ProcessRequest()方法中重复使用的实例化代码进行重构，放到@Before中
+    */
     @Before
     public void instanitate() throws Exception {
         System.out.println("这里是第一个@Before");
         controller = new DefaultController();
+        request = new SampleRequest();
+        handler = new SampleHandler();
+        controller.AddHandler(request, handler);
     }
 
     @After
@@ -45,7 +53,7 @@ public class DefaultControllerTest {
         System.out.println("这是第一个after.........");
     }
 
-    /*针对Controller的请求处理扩展进行测试*/
+    /*针对Controller的请求处理扩展进行测试*//*
     @Test
     public void AddHandlerTest() {
         Request request = new SampleRequest();
@@ -55,11 +63,11 @@ public class DefaultControllerTest {
         assertSame("获取到的requestHandler应该要和设置的一样", requestHandler1, requestHandler);
     }
 
-    /*
+    *//*
      针对Controller的核心功能：请求处理进行测试
     1、处理器返回的Respone是否为NULL
     2、处理器在收到请求后，判断返回的Respone是否与RequestHandler返回的Respone是同一对象
-    */
+    *//*
     @Test
     public void processRequestTest() {
         Request request = new SampleRequest();
@@ -68,6 +76,19 @@ public class DefaultControllerTest {
         Response response = controller.processRequest(request);
         assertNotNull("不能返回一个空的Respone对象", response);
         assertEquals("respone应该是SampleRespone的一个对象", response.getClass(), SampleRespone.class);
+    }*/
+
+    @Test
+    public void AddHandlerTest() {
+        RequestHandler handler2 = controller.getHandler(request);
+        assertSame(handler2, handler);
+    }
+
+    @Test
+    public void ProcessRequest() {
+        Response response = controller.processRequest(request);
+        assertNotNull(response);
+        assertEquals(SampleRespone.class, response.getClass());
     }
 
 }
